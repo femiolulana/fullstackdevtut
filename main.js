5953d327749d554e8e190e53793bd82535c2b748
femiolulana <oniseikeji@gmail.com>
2024-07-06 03:18:57 -0400

    Your commit message

---
+++ main.js
@@ -0,0 +1,24 @@
+/* Create Varaibles to be used in RPG Game */
+
+let experencePoint = 0
+let health = 100
+let gold = 50
+let currentWeapon = 0
+fight;
+let monsterHealth
+let inventory = ["stick", "dagger","sword"]
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xptext = document.querySelector("#xptext");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterNameText = document.querySelector("#monsterNameText");
+const monsterHealthText = document.querySelector("#monsterHealthText");
+
+// 
+
+
+ 
